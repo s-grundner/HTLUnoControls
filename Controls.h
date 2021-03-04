75afef9909e2e8d5c4a7a0374f44e78d981227f5
Smon <55248627+s-grundner@users.noreply.github.com>
2021-03-04 18:08:37 +0100

    docu

--- Controls.h
+++ Controls.h
@@ -45,22 +45,22 @@ public:
 	void setLine(bool b);
 	void setRgb(bool b);
 
-	void setMode(int n);					//input a 3-bit binary value referring to the corresponding DDR bits of the Onboard Control pins 12 11 10
-	int getMode();							//returns a 3-bit value which is cut out from the DDR onboard-control section e.i.: getMode(0b000 000 00) the 3 standalone bits equals the return value
+	void setMode(int n);			//input a 3-bit binary value referring to the corresponding DDR bits of the Onboard Control pins 12 11 10
+	int getMode();				//returns a 3-bit value which is cut out from the DDR onboard-control section e.i.: getMode(0b000 000 00) the 3 standalone bits equals the return value
 
-	void ledOn(bool b);						//outputs HI on pins 0-9
-	void setLed(unsigned long x);			//outputs HI or LO on pins 0-9 according to the user 10-bit binary input e.i.: setLed(0b000011111) led 0-4 are HI led 5-9 are LO
+	void ledOn(bool b);			//outputs HI on pins 0-9
+	void setLed(unsigned long x);		//outputs HI or LO on pins 0-9 according to the user 10-bit binary input e.i.: setLed(0b000011111) led 0-4 are HI led 5-9 are LO
 	
-	void setNumber(int n);					//displays a hex number from -31 to 31 if hex display is active or a 10-bit binary number if the led line is active
-	void setLetter(char l); 				//displays alphabet character on hex display
-	void Write(char s[]);					//not yet implemented
+	void setNumber(int n);			//displays a hex number from -31 to 31 if hex display is active or a 10-bit binary number if the led line is active
+	void setLetter(char l); 		//displays alphabet character on hex display
+	void Write(char s[]);			//not yet implemented
 
 	double getPercentageAnalogIn(int n);	//reads analog input-pin and returns the percentage of the applied voltage while 5V are 100%
-	void showPercentageAnalogIn(int n);		//reads analog input-pin and displays it on the in decades, if the hex display is active, or indicates the percentage on the led-bar if the led-bar is active
+	void showPercentageAnalogIn(int n);	//reads analog input-pin and displays it on the in decades, if the hex display is active, or indicates the percentage on the led-bar if the led-bar is active
 	void showPan(int n);
 
-	void Color(long n);						//input RGB-hex value in hex
-	void Mux(int * n);						//not yet implemented
+	void Color(long n);			//input RGB-hex value in hex
+	void Mux(int * n);			//not yet implemented
 
 private:
 	bool pin_active;
